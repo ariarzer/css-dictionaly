64f8817d3787ba4aad09be344799c83cfdde497b
Sofi Valitova <ariarzer@gmail.com>
2022-02-11 18:03:51 +0300

    separate term into the folder by spec name

--- scripts/build_complete_list.js
+++ scripts/build_complete_list.js
@@ -8,9 +8,13 @@ const __dirname = new URL('.', import.meta.url).pathname;
 
 const termsList = {};
 
-fs.readdirSync(path.join(__dirname, '../terms')).forEach((fileName) => {
-    const term = JSON.parse(fs.readFileSync(path.join(__dirname, '../terms', fileName)).toString());
-    termsList[fileName.slice(0, -5)] = term;
+fs.readdirSync(path.join(__dirname, '../terms')).forEach((dirName) => {
+    if (!path.extname(dirName)) {
+        fs.readdirSync(path.join(__dirname, '../terms', dirName)).forEach((fileName) => {
+            const term = JSON.parse(fs.readFileSync(path.join(__dirname, '../terms', dirName, fileName)).toString());
+            termsList[fileName.slice(0, -5)] = term;
+        });
+    }
 });
 
 fs.writeFileSync(path.join(__dirname, '../dist/terms_list.json'), JSON.stringify(termsList));
